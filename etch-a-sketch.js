3c3806dea8374dc223ab863d0a4cee365f5d2419
Ari <chan.ari96@gmail.com>
2022-04-09 16:02:27 -0400

    Add reset and size change functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,51 @@
-const x = new Array(16);
-for (let i = 0; i < x.length; i++) {
-    x[i] = document.createElement('div');
-    x[i].classList.add('row');
-    const y = new Array(16);
-    for (let j = 0; j < y.length; j++){
-        y[j] = document.createElement('div');
-        y[j].classList.add('square');
-        x[i].appendChild(y[j]);
+function generateGrid(size) {
+    const x = new Array(size);
+    for (let i = 0; i < x.length; i++) {
+        x[i] = document.createElement('div');
+        x[i].classList.add('row');
+        const y = new Array(size);
+        for (let j = 0; j < y.length; j++){
+            y[j] = document.createElement('div');
+            y[j].classList.add('square');
+            x[i].appendChild(y[j]);
+        }
+    }
+
+    const container = document.querySelector('#container');
+    for (let i = 0; i < x.length; i++) {
+        container.appendChild(x[i]);
     }
 }
 
-const container = document.querySelector('#container');
-for (let i = 0; i < x.length; i++) {
-    container.appendChild(x[i]);
+function changeSquare() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            if (square.style['background-color'] === "black") {
+                    square.style['background-color'] = "white";
+            }
+            else {
+                square.style['background-color'] = "black";
+            }
+        });
+    });
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        if (square.style['background-color'] === "black") {
-            square.style['background-color'] = "white";
-        }
-        else {
-            square.style['background-color'] = "black";
-        }
-    });
+function clearGrid() {
+    const container = document.querySelector('#container');
+    let children = container.children;
+    for (let i = children.length - 1; i >= 0; i--) {
+        container.removeChild(children[i]);
+    }
+}
+
+generateGrid(16);
+changeSquare();
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    let size = parseInt(prompt("Enter desired grid size: "));
+    clearGrid();
+    generateGrid(size);
+    changeSquare();
 });
